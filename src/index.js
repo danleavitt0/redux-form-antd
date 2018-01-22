205b86a83c1651ea0fdaa074e5e5f3b3361a4c04
danleavitt0 <danleavitt0@gmail.com>
2018-01-22 10:02:26 -0800

    FIRST

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { Checkbox, Input, Slider, InputNumber, Switch } from "antd";
 import createComponent from "./components/BaseComponent";
 
-import {
-  RadioField as Radio,
-  SelectField as Select
-} from "./components/MultiSelect";
-import mapError, { customMap, getValidateStatus} from "./components/mapError";
+import { RadioField as Radio, SelectField as Select } from "./components/MultiSelect";
+import mapError, { customMap, getValidateStatus } from "./components/mapError";
 const CheckboxGroup = Checkbox.Group;
 
 const defaultTo = (value, d) => {
@@ -22,55 +19,40 @@ const checkboxGroupMap = customMap(({ input: { onChange, value = [] } }) => {
   return { onChange, value };
 });
 
-const sliderMap = customMap(
-  ({ input: { onChange, value = 0 }, range, min = 0, max = 100 }) => {
-    value = defaultTo(value, range ? [min, max] : 0);
-    return { onAfterChange: onChange, value };
-  }
-);
+const sliderMap = customMap(({ input: { onChange, value = 0 }, range, min = 0, max = 100 }) => {
+  value = defaultTo(value, range ? [min, max] : 0);
+  return { onAfterChange: onChange, value };
+});
 const textFieldMap = customMap(({ input: { onChange } }) => ({
   onChange: v => onChange(v.nativeEvent.target.value)
 }));
 
-const selectFieldMap = customMap(
-  ({ input: { onChange, value }, multiple, options }) => {
-    if (options && options.length > 0) {
-      value = value ? value : multiple ? [options[0].value] : options[0].value;
-    }
-    return { dropdownMatchSelectWidth: true, value, style: { minWidth: 200 } };
+const selectFieldMap = customMap(({ input: { onChange, value }, multiple, options }) => {
+  if (options && options.length > 0) {
+    value = value ? value : multiple ? [options[0].value] : options[0].value;
   }
-);
+  return { dropdownMatchSelectWidth: true, value, style: { minWidth: 200 } };
+});
 
-const bluredFieldMap = ({
-    meta: {touched, error, warning, valid} = {},
-    input: {value, onChange},
-    ...props
-  }) => ({
-    ...props,
-    defaultValue: value,
-    onBlur: e => {
-      onChange(e.nativeEvent.target.value);
-    },
-    validateStatus: getValidateStatus(touched, error, warning, valid),
-    help: touched && (error || warning)
-  });
+const bluredFieldMap = ({ meta: { touched, error, warning, valid } = {}, input: { value, onChange }, ...props }) => ({
+  ...props,
+  defaultValue: value,
+  onBlur: e => {
+    onChange(e.nativeEvent.target.value);
+  },
+  validateStatus: getValidateStatus(touched, error, warning, valid),
+  help: touched && (error || warning)
+});
 
-
-const switchMap = customMap(({input: {value}}) => ({
+const switchMap = customMap(({ input: { value } }) => ({
   checked: value
 }));
 
-export const CheckboxGroupField = createComponent(
-  CheckboxGroup,
-  checkboxGroupMap
-);
+export const CheckboxGroupField = createComponent(CheckboxGroup, checkboxGroupMap);
 
 // will trigger on change only onBlur
 // usefull for performance reasons
-export const LazyTextField = createComponent(
-  Input,
-  bluredFieldMap
-);
+export const LazyTextField = createComponent(Input, bluredFieldMap);
 export const SelectField = createComponent(Select, selectFieldMap);
 export const CheckboxField = createComponent(Checkbox, eventMap);
 export const RadioField = createComponent(Radio, eventMap);
